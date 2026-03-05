2a1d173acea59c6b5cca21e5cfd4f16f8ed3d3dc
Ida Sharon <idasharon.r@gmail.com>
2026-03-05 15:00:42 +0530

    Add unit tests for calculator operations
    Added unit tests for addition, subtraction, multiplication, and division methods.

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,1 +1,30 @@
 
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    App calc = new App();
+
+    @Test
+    public void testAddition() {
+        assertEquals(15, calc.add(10, 5));
+    }
+
+    @Test
+    public void testSubtraction() {
+        assertEquals(5, calc.subtract(10, 5));
+    }
+
+    @Test
+    public void testMultiplication() {
+        assertEquals(50, calc.multiply(10, 5));
+    }
+
+    @Test
+    public void testDivision() {
+        assertEquals(2, calc.divide(10, 5));
+    }
+}
