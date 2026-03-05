46df4ff449d68ed4f2e2535f25b6b08fa6ac4def
Ida Sharon <idasharon.r@gmail.com>
2026-03-05 15:00:07 +0530

    Implement basic calci in App class

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,1 +1,27 @@
 
+package com.example;
+
+public class App {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        App calc = new App();
+        int result = calc.add(10, 5);
+        System.out.println("Addition Result: " + result);
+    }
+}
